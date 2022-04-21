46d718f71abf8bc48958e8e3815f83b902f16ddd
Szewieczek <helena.szewieczek@gmail.com>
2022-04-21 22:03:53 +0200

    Metody pro kreslení rovnostranného trojuhelníku, čtverce, obdelníku a kolečka

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,57 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
-
         zofka = new Turtle();
+        zofka.setPenColor(Color.magenta);
+        zofka.setLocation(400, 400);
+
+        Turtle bonifac;
+        bonifac = new Turtle();
+        bonifac.setPenColor(Color.cyan);
+        bonifac.setLocation(800, 400);
+
+        nakresliKolecko(zofka, 400);
+        nakresliObdelnik(bonifac, 200, 50);
+        nakresliRovnostrannyTrojuhelnik(zofka, 150);
+        nakresliCtverec(bonifac, 90);
+
     }
 
-}
+    private void nakresliRovnostrannyTrojuhelnik(Turtle turtle, double delkaStrany) {
+        for (int i = 0; i < 3; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(120);
+        }
+    }
+
+    private void nakresliCtverec(Turtle turtle, double delkaStrany) {
+        for (int i = 0; i < 4; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(90);
+        }
+    }
+
+    private void nakresliObdelnik(Turtle turtle, double stranaA, double stranaB) {
+        for (int i = 0; i < 2; i++) {
+            turtle.move(stranaA);
+            turtle.turnRight(90);
+            turtle.move(stranaB);
+            turtle.turnRight(90);
+        }
+    }
+
+    private void nakresliKolecko(Turtle turtle, double obvod) {
+        double delkaStrany = obvod / 36;
+        for (int i = 0; i < 36; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight (10);
+            }
+        }
+    }
+
